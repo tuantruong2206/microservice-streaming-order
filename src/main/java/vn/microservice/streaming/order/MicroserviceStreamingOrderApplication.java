b0f233ce023e9748c2be6d629c92891ee2183d80
tuan.truong@metanet.co.kr <tuan.truong@metanet.co.kr>
2020-12-29 18:12:36 +0700

    initial commit

---
+++ src/main/java/vn/microservice/streaming/order/MicroserviceStreamingOrderApplication.java
@@ -0,0 +1,13 @@
+package vn.microservice.streaming.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStreamingOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStreamingOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/microservice/streaming/order/MicroserviceStreamingOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.microservice.streaming.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStreamingOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
